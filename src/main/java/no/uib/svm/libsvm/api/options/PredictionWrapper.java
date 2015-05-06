8c4d0c1c7e22438ec6ce58c3c3f63492ce228316
Kristian Hestetun <ksh@driw.no>
2015-05-06 20:32:04 +0200

    Started on wrapper for svm_predict

---
+++ src/main/java/no/uib/svm/libsvm/api/options/PredictionWrapper.java
@@ -0,0 +1,16 @@
+package no.uib.svm.libsvm.api.options;
+
+/**
+ * Created by kristianhestetun on 06.05.15.
+ */
+public class PredictionWrapper {
+
+    public void loadModel(String filePath){
+        // load model from file
+    }
+
+    public void loadTestData(String filePath){
+        // load test data from file
+    }
+
+}
