ed463efadbf2ace97d29c0334d2b008be5b4ae93
Kristian Hestetun <ksh@driw.no>
2015-05-08 15:21:20 +0200

    Continued work on the prediction wrapper

--- src/main/java/no/uib/svm/libsvm/api/options/PredictionWrapper.java
+++ src/main/java/no/uib/svm/libsvm/api/options/PredictionWrapper.java
@@ -1,16 +1,63 @@
 package no.uib.svm.libsvm.api.options;
 
+import no.uib.svm.libsvm.core.libsvm.svm;
+import no.uib.svm.libsvm.core.libsvm.svm_model;
+import no.uib.svm.libsvm.core.svm_predict;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Logger;
+
 /**
  * Created by kristianhestetun on 06.05.15.
  */
 public class PredictionWrapper {
 
-    public void loadModel(String filePath){
-        // load model from file
+    private String
+            modelFilePath,
+            testDataFilePath;
+    private svm_model model;
+
+    private Boolean predictProbability = false;
+
+    public void loadModel(String filePath) throws IOException {
+        model = svm.svm_load_model(filePath);
     }
 
-    public void loadTestData(String filePath){
-        // load test data from file
+    public void predict() {
+        BufferedReader inputReader = getTestDataReader();
+
+        svm_predict.predict(
+                inputReader, outputStream, this.model, predictProbability ? 1 : 0);
+    }
+
+    public BufferedReader getTestDataReader() {
+        if (testDataFilePath != null && !testDataFilePath.isEmpty()) {
+            try {
+                return new BufferedReader(new FileReader(testDataFilePath));
+            } catch (FileNotFoundException e) {
+                Logger.getAnonymousLogger().info("couldnt find specified file.." + testDataFilePath);
+            }
+        }
+        return null;
+    }
+
+    public String getModelFilePath() {
+        return modelFilePath;
+    }
+
+    public void setModelFilePath(String modelFilePath) {
+        this.modelFilePath = modelFilePath;
+    }
+
+    public void setTestDataFilePath(String filePath) {
+        this.testDataFilePath = filePath;
+    }
+
+    public String getTestDataFilePath() {
+        return testDataFilePath;
     }
 
 }
--- src/main/java/no/uib/svm/libsvm/core/svm_predict.java
+++ src/main/java/no/uib/svm/libsvm/core/svm_predict.java
@@ -5,7 +5,7 @@ import no.uib.svm.libsvm.core.libsvm.*;
 import java.io.*;
 import java.util.StringTokenizer;
 
-class svm_predict {
+public class svm_predict {
 	private static svm_print_interface svm_print_null = new svm_print_interface()
 	{
 		public void print(String s) {}
@@ -36,7 +36,7 @@ class svm_predict {
 		return Integer.parseInt(s);
 	}
 
-	private static void predict(BufferedReader input, DataOutputStream output, svm_model model, int predict_probability) throws IOException
+	public static void predict(BufferedReader input, DataOutputStream output, svm_model model, int predict_probability) throws IOException
 	{
 		int correct = 0;
 		int total = 0;
