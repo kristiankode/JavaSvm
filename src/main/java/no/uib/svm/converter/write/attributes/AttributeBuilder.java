91047c8c904e73cb7b670e0b3f30f5389ac48f24
Kristian Hestetun <ksh@driw.no>
2015-10-02 10:51:28 +0200

    Added more javadoc

--- src/main/java/no/uib/svm/converter/write/attributes/AttributeBuilder.java
+++ src/main/java/no/uib/svm/converter/write/attributes/AttributeBuilder.java
@@ -5,9 +5,16 @@ import no.uib.svm.converter.domain.Genome;
 import java.util.List;
 
 /**
- * @author kristian
- *         Created 03.09.15.
+ * Creates a numerical representation of the attributes of a genome.
  */
 public interface AttributeBuilder {
+
+    /**
+     * A list of numeric values that describes the attributes of a genome.
+     * The classification is NOT included in this list.
+     *
+     * @param genome The genome to get attributes for.
+     * @return List of attributes as numbers.
+     */
     List<Double> getAttributeValues(Genome genome);
 }
