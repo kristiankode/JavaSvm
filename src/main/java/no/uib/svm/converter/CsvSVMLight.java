d2fa606a2fe19d793c38e1e3f3a1fbbb3e608b47
Kristian Hestetun <ksh@driw.no>
2015-10-02 10:44:32 +0200

    Converter now takes input and output path as program arguments

--- src/main/java/no/uib/svm/converter/CsvSVMLight.java
+++ src/main/java/no/uib/svm/converter/CsvSVMLight.java
@@ -6,36 +6,34 @@ import no.uib.svm.converter.read.CsvReader;
 import no.uib.svm.converter.write.attributes.AttributeBuilder;
 import no.uib.svm.converter.write.attributes.SubstringAsFeature;
 import no.uib.svm.converter.write.attributes.SubstringOccurenceAsFeature;
-import no.uib.svm.converter.write.destination.OutputWriter;
-import no.uib.svm.converter.write.destination.WriterFactory;
+import no.uib.svm.converter.write.destination.FileWriter;
 import no.uib.svm.converter.write.row.RowBuilder;
-import no.uib.svm.libsvm.core.settings.Settings;
-import no.uib.svm.libsvm.core.settings.SettingsFactory;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
+/**
+ * Converts a CSV file to SVM-light format.
+ */
 public class CsvSVMLight {
 
-    private static final String
-            INPUT_FILE = "/Users/kristianhestetun/Utvikling/MachineLearning/JavaSVM/200k.csv";
-
-    private OutputWriter outputWriter = WriterFactory.getWriter();
     private AttributeBuilder attributeBuilder = new SubstringOccurenceAsFeature();
 
-    public CsvSVMLight()
-            throws FileNotFoundException, UnsupportedEncodingException {
-    }
-
+    /**
+     * Starts the program with input and output arguments.
+     *
+     * @param args Path of input file,
+     *             path of output file. Note: Use only existing directories, no new folders will be created.
+     * @throws FileNotFoundException
+     * @throws UnsupportedEncodingException
+     */
     public static void main(String[] args)
             throws FileNotFoundException, UnsupportedEncodingException {
         CsvSVMLight csvSVMLight = new CsvSVMLight();
-        csvSVMLight.run();
-    }
-
-    private void run() {
-        convertToSvmFormat(INPUT_FILE);
+        String inputFile = args[0];
+        String outputFile = args[1];
+        csvSVMLight.convertToSvmFormat(inputFile, outputFile);
     }
 
     /**
@@ -43,10 +41,11 @@ public class CsvSVMLight {
      *
      * @param inputFilePath Path to csv file
      */
-    private void convertToSvmFormat(String inputFilePath) {
+    private void convertToSvmFormat(String inputFilePath, String outputPath) {
         try {
             CsvReader csvReader = new BufferedCsvReader(inputFilePath);
             RowBuilder rowBuilder = new RowBuilder(attributeBuilder);
+            FileWriter outputWriter = new FileWriter(outputPath);
 
             String[] rowData;
             while ((rowData = csvReader.readNextRow()) != null) {
